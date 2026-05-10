a59e94a76f3acee1d9d782694c19d5c6723bfcb5
faraulia <frhkzara@gmail.com>
2026-05-10 10:42:10 +0700

    em ini apus usestate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
 // import "./App.css";
